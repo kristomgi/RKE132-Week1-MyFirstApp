13d4b8f63d73b3804372a722d9094f24dfadb0f4
Kristo Magi <Kristo8magi@gmail.com>
2023-09-17 21:11:53 +0300

    create new app

---
+++ MyFisrtApp/Program.cs
@@ -0,0 +1,13 @@
+﻿//This is a comment to my code
+
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //Output
+//STRING - sõna
+string userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+
+// string interpolatino
+Console.WriteLine($"Hello, {userName} !"); //Output
